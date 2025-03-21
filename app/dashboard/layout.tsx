e639a0fcdfaa690eee1d940fcbe02b2bcba35fe4
Bhati90 <95360867+Bhati90@users.noreply.github.com>
2025-03-21 13:48:56 +0530

    Add files via upload

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
+import { NavigationProvider } from "@/lib/context/navigation";
+import { Authenticated } from "convex/react";
+import { MessageBubble } from "@/components/MessageBubble";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <NavigationProvider>
+      <div className="flex h-screen">
+        <Authenticated>
+          <Sidebar />
+          {/* <MessageBubble /> */}
+        </Authenticated>
+
+        <div className="flex-1 flex flex-col min-w-0">
+          <Header />
+          {/* <Sidebar /> */}
+
+          <main className="flex-1 overflow-y-auto">{children}</main>
+        </div>
+      </div>
+    </NavigationProvider>
+  );
+}
